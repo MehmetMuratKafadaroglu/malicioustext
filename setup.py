fdc4ba1d7fadbdeb56143cc7c66053abd91f8899
MehmetMuratKafadaroglu <74990545+MehmetMuratKafadaroglu@users.noreply.github.com>
2022-12-03 21:51:51 +0000

    'v'

--- setup.py
+++ setup.py
@@ -1,21 +1,34 @@
 from setuptools import setup
+from setuptools.command.install import install
 
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        import nltk
+        install.run(self)
+        print("Downloading nltk")
+        nltk.download('stopwords')
+        nltk.download('omw-1.4')
+        nltk.download('punkt')
+        nltk.download('wordnet')
+        
 setup(
-    name='malicioustext',
-    version='0.1.0',    
-    description='Python package that finds malicious text',
-    url='https://github.com/MehmetMuratKafadaroglu/malicioustext',
+    name='offensivetext',
+    version='0.2.0',    
+    description='Filter out racism, sexism and sexual content with pretrained models',
+    url='https://github.com/MehmetMuratKafadaroglu/offensivetext',
     author='Mehmet Kafadaroglu',
     author_email='mehmetkafadaroglu@gmail.com',
     license='BSD 2-clause',
-    packages=['malicioustext'],
-    install_requires=['sklearn',
+    packages=['offensivetext'],
+    install_requires=['sklearn',                    'scikit-learn',
                     'numpy', 
-                    'nltk',
-                      ],
-
+                    'nltk',                    'pandas',                    'scipy',                    'matplotlib',                      ],    package_data = {'': ['models/*.sav']},
+    include_package_data = True,
     classifiers=[
        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-    ],
+    ],    cmdclass={
+        'install': PostInstallCommand,
+    },
 )
